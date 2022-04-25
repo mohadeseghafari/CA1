180e717b72e95ab6c7b503e27833559e4f12f078
mojak <javadakbari1379@gmail.com>
2022-04-25 16:35:09 +0430

    debug

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import datetime
-import string
 from time_extractions import TimeExtraction
+from datetime import timedelta
 import utility
 from datetime import date
 import re
@@ -10,7 +10,7 @@ def interval_decoder(span):
     return [int(span[0]), int(span[1])]
 
 def test():
-    print(run("من و علی دوشنبه ساعت سه بازی کنیم"))
+    print(run("من و تو هر هفته به فوتبال میرویم"))
 
 def run(string: str):
     functions = [crontime_handler, time_interval_handler, exact_handler]
@@ -20,7 +20,7 @@ def run(string: str):
             return result
     return None
 def sum_time(sign,h,m,s):
-    now = datetime.now() + sign* timedelta(hours = h, minutes = m, seconds = s )
+    now = datetime.now() + sign*  timedelta(hours = h, minutes = m, seconds = s )
     return now.time()
 
 def handle_time(time_str):
@@ -108,31 +108,31 @@ def time_interval_handler(string):
 
         tmp = extractor.run(first_time)['values']['date']
         d1 = date.today()
-        for span, string in tmp.items():
-            d1 = utility.date_decod(string)
+        for span, datestring in tmp.items():
+            d1 = utility.date_decod(datestring)
             first_span = interval_decoder(span)
             first_span[0] += az_index + interval_decoder(x)[0]
             first_span[1] += az_index + interval_decoder(x)[0]
         tmp2 = extractor.run(first_time)['values']['time']
 
         t1 = datetime.time(0, 0, 0, 0)
-        for span, string in tmp2.items():
-            t1 = handle_tme(string)
+        for span, timestring in tmp2.items():
+            t1 = handle_time(timestring)
         
         d1 = datetime.datetime.combine(d1, t1)
         d2 = date.today()
                                
         tmp = extractor.run(second_time)['values']['date']
-        for span, string in tmp.items():
-            d2 = utility.date_decod(string)
+        for span, datestring in tmp.items():
+            d2 = utility.date_decod(datestring)
             second_span = interval_decoder(span)
             second_span[0] += ta_index + 3 + interval_decoder(x)[0]
             second_span[1] += ta_index + 3 + interval_decoder(x)[0]
         tmp2 = extractor.run(second_time)['values']['time']
 
         t2 = datetime.time(0, 0, 0, 0)
-        for span, string in tmp2.items():
-            t2 = handle_tme(string)
+        for span, timestring in tmp2.items():
+            t2 = handle_time(timestring)
 
         d2 = datetime.datetime.combine(d2, t2)
         output['value'] = [d1.timestamp(), d2.timestamp()]
@@ -156,12 +156,13 @@ def crontime_handler(string):
         output['span'] = x
                                
         time ='' 
-        for span, string in tmp.items():
-            time = string                     
+        tmp2 = extractor.run(result[x])['values']['time']
+        for span, timestring in tmp2.items():
+            time = timestring                     
         time = ctime(time)
         tmp = extractor.run(result[x])['values']['date']
-        for span, string in tmp.items():
-            array = string.split(' ')
+        for span, datestring in tmp.items():
+            array = datestring.split(' ')
             for i in range(len(array) ) :
                 if array[i] == 'هر' :
                     break
@@ -182,7 +183,7 @@ def crontime_handler(string):
             elif array[i+1] == 'جمعه' :
                 return time + ' * 0 5'
             elif array[i+1] == 'ماه' :
-                x = re.findall('[0-9]+', string)
+                x = re.findall('[0-9]+', datestring)
                 return time + ' * ' + x[0] + ' *'
 
 def exact_handler(string):
@@ -203,8 +204,8 @@ def exact_handler(string):
     tmp2 = extractor.run(string)['values']['time']
     print(tmp2)
     t1 = datetime.time(0, 0, 0, 0)
-    for span, string in tmp2.items():
-        t1 = handle_tme(string)
+    for span, timestring in tmp2.items():
+        t1 = handle_time(timestring)
         
     
     d1 = datetime.datetime.combine(d1, t1)
